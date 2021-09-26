75508ba35fdafbd7681be2f63a170b3ab6b2fa13
Miguel angel Munoz fernandez <mmunoz-f@c1r11s2.42madrid.com>
2021-09-26 15:09:35 +0200

    26/09/2021 15:09:35 - cpp08 done

--- cpp_08/ex00/easyfind.hpp
+++ cpp_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: mmunoz-f <mmunoz-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 08:32:44 by mmunoz-f          #+#    #+#             */
-/*   Updated: 2021/09/24 08:53:33 by mmunoz-f         ###   ########.fr       */
+/*   Updated: 2021/09/26 15:05:00 by mmunoz-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 template<typename T>
 bool	easyfind(const T &container, const int n) {
-	std::T<int>::iterator	it = std::find(container.begin(), container.last(), n);
+	const typename T::const_iterator	it = std::find(container.begin(), container.end(), n);
 	if (it == container.end())
 		return (false);
 	return (true);
--- cpp_08/ex00/main.cpp
+++ cpp_08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mmunoz-f <mmunoz-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 08:54:44 by mmunoz-f          #+#    #+#             */
-/*   Updated: 2021/09/24 09:09:37 by mmunoz-f         ###   ########.fr       */
+/*   Updated: 2021/09/26 15:07:40 by mmunoz-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ int	main(void) {
 
 	std::list<int>					lst;
 	std::vector<int>				v;
-	std::map<int, std::string>		s;
 
 	for (int i = 0; i < 10; i++) {
 		lst.push_back(i);
 		v.push_back(i);
 	}
-
-
+	std::cout << (easyfind(lst, 3) ? "true" : "false") << std::endl;
+	std::cout << (easyfind(v, 11) ? "true" : "false") << std::endl;
+	return (0);
 }
